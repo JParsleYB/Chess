13c8f2ec9259ef48d2b7307c5752a8581ecf8899
ParsleY <jparsleyb@gmail.com>
2023-05-21 17:18:25 +0200

    change px units to vh in css file

--- js.js
+++ js.js
@@ -47,12 +47,43 @@ window.addEventListener("DOMContentLoaded", function() {
       });
     });
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    setPieceStartPosition();
 
-  
-
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    console.log(coordinates);
+    function setPieceStartPosition(){
+        setPieceCoords("wk", coordinates["d1"]);
+        setPieceCoords("bk", coordinates["d8"]);
+        setPieceCoords("wq", coordinates["e1"]);
+        setPieceCoords("bq", coordinates["e8"]);
+        setPieceCoords("wb", coordinates["c1"]);
+        setPieceCoords("bb", coordinates["c8"]);
+        setPieceCoords("wb2", coordinates["f1"]);
+        setPieceCoords("bb2", coordinates["f8"]);
+        setPieceCoords("wkn", coordinates["b1"]);
+        setPieceCoords("bkn", coordinates["b8"]);
+        setPieceCoords("wkn2", coordinates["g1"]);
+        setPieceCoords("bkn2", coordinates["g8"]);
+        setPieceCoords("wr", coordinates["a1"]);
+        setPieceCoords("br", coordinates["a8"]);
+        setPieceCoords("wr2", coordinates["h1"]);
+        setPieceCoords("br2", coordinates["h8"]);
+        setPieceCoords("wp", coordinates["a2"]);
+        setPieceCoords("wp2", coordinates["b2"]);
+        setPieceCoords("wp3", coordinates["c2"]);
+        setPieceCoords("wp4", coordinates["d2"]);
+        setPieceCoords("wp5", coordinates["e2"]);
+        setPieceCoords("wp6", coordinates["f2"]);
+        setPieceCoords("wp7", coordinates["g2"]);
+        setPieceCoords("wp8", coordinates["h2"]);
+        setPieceCoords("bp", coordinates["a7"]);
+        setPieceCoords("bp2", coordinates["b7"]);
+        setPieceCoords("bp3", coordinates["c7"]);
+        setPieceCoords("bp4", coordinates["d7"]);
+        setPieceCoords("bp5", coordinates["e7"]);
+        setPieceCoords("bp6", coordinates["f7"]);
+        setPieceCoords("bp7", coordinates["g7"]);
+        setPieceCoords("bp8", coordinates["h7"]);
+        console.log("ustawiono pozycje startowe figur");
+    }
   
     function setPieceCoords(pieceId, coords) {
       var piece = document.getElementById(pieceId);
@@ -76,38 +107,8 @@ window.addEventListener("DOMContentLoaded", function() {
       selectedPieceId = null;
     }
 
-    setPieceCoords("wk", coordinates["d1"]);
-    setPieceCoords("bk", coordinates["d8"]);
-    setPieceCoords("wq", coordinates["e1"]);
-    setPieceCoords("bq", coordinates["e8"]);
-    setPieceCoords("wb", coordinates["c1"]);
-    setPieceCoords("bb", coordinates["c8"]);
-    setPieceCoords("wb2", coordinates["f1"]);
-    setPieceCoords("bb2", coordinates["f8"]);
-    setPieceCoords("wkn", coordinates["b1"]);
-    setPieceCoords("bkn", coordinates["b8"]);
-    setPieceCoords("wkn2", coordinates["g1"]);
-    setPieceCoords("bkn2", coordinates["g8"]);
-    setPieceCoords("wr", coordinates["a1"]);
-    setPieceCoords("br", coordinates["a8"]);
-    setPieceCoords("wr2", coordinates["h1"]);
-    setPieceCoords("br2", coordinates["h8"]);
-    setPieceCoords("wp", coordinates["a2"]);
-    setPieceCoords("wp2", coordinates["b2"]);
-    setPieceCoords("wp3", coordinates["c2"]);
-    setPieceCoords("wp4", coordinates["d2"]);
-    setPieceCoords("wp5", coordinates["e2"]);
-    setPieceCoords("wp6", coordinates["f2"]);
-    setPieceCoords("wp7", coordinates["g2"]);
-    setPieceCoords("wp8", coordinates["h2"]);
-    setPieceCoords("bp", coordinates["a7"]);
-    setPieceCoords("bp2", coordinates["b7"]);
-    setPieceCoords("bp3", coordinates["c7"]);
-    setPieceCoords("bp4", coordinates["d7"]);
-    setPieceCoords("bp5", coordinates["e7"]);
-    setPieceCoords("bp6", coordinates["f7"]);
-    setPieceCoords("bp7", coordinates["g7"]);
-    setPieceCoords("bp8", coordinates["h7"]);
-
+    
   });
+
+  
   
